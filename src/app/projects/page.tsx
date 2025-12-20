be383e783ebddb7dc21f755ae62dd9ea3e035f0f
Ritvik Ellendula <ritvikellendula@Ritviks-MacBook-Pro.local>
2025-12-20 17:48:10 -0500

    Fix projects page data and add course project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -9,35 +9,45 @@ const PROJECTS: MediaCardItem[] = [
     blurb:
       "A custom-built personal website designed to showcase my projects and interests. Focused on interactive UI, reusable components, and production deployment using modern web tools.",
     href: "/projects/website",
-    tags: ["React", "Next.js", "Frontend","UI/UX","Git","Deployment"],
+    tags: ["React", "Next.js", "Frontend", "UI/UX", "Git", "Deployment"],
     media: { kind: "image", src: "/projects/website.png", alt: "Website" },
   },
+
   {
-  id: "002",
-  type: "independent",
-  eyebrow: "CAD",
-  title: "3D Printed Interactive Brain Model",
-  blurb: "A high school personal project, where I designed an accurate model of the brain, including the various gyri and sulci through Maya, and added physical labels as well.",
-  href: "/projects/brain",
-  tags: ["CAD", "Maya", "Neuroscience", "Anatomical Modeling"],
-  media: {
-    kind: "image",
-    src: "/projects/brain.png",
-    alt: "3D printed brain model",
+    id: "002",
+    type: "independent",
+    eyebrow: "CAD",
+    title: "3D Printed Interactive Brain Model",
+    blurb:
+      "A high school personal project, where I designed an accurate model of the brain, including the various gyri and sulci through Maya, and added physical labels as well.",
+    href: "/projects/brain",
+    tags: ["CAD", "Maya", "Neuroscience", "Anatomical Modeling"],
+    media: {
+      kind: "image",
+      src: "/projects/brain.png",
+      alt: "3D printed brain model",
+    },
   },
-}
 
   // Example of a course project later:
-  // { id:"003", type:"course", ... }
+  // { id:"004", type:"course", ... }
 
   {
     id: "003",
     type: "course",
     eyebrow: "Data Analysis",
-    title: "A Data Analysis Exploration into Environmental & Socioeconomic Factors on Poor Health Outcomes",
-    blurb: "A final presentation for my STAT 7770 course, where we analyzed the influence of various socioeconomic factors on health, utilizing numerous python libraries such as pandas, seaborn, numpy, etc. ",
-    href: "/projects/brain",
-    tags: ["Python", "Numpy", "Pandas","Data Pre-Processing","Decision Trees"],
+    title:
+      "A Data Analysis Exploration into Environmental & Socioeconomic Factors on Poor Health Outcomes",
+    blurb:
+      "A final presentation for my STAT 7770 course, where we analyzed the influence of various socioeconomic factors on health, utilizing numerous python libraries such as pandas, seaborn, numpy, etc.",
+    href: "/projects/oidd",
+    tags: [
+      "Python",
+      "NumPy",
+      "Pandas",
+      "Data Pre-Processing",
+      "Decision Trees",
+    ],
     media: {
       kind: "image",
       src: "/projects/OIDD.png",
